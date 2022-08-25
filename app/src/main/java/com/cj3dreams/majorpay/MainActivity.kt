c43d8f971376120a1b13eedf3a273186d0be27a8
3Dimensional <cj3dream@gmail.com>
2022-08-26 00:28:38 +0500

    Huge tasks were done
    -Main UI is ready almost
    -Added all needed dependencies
    -Designed icon and some views
    -Android Manifest settings set up
    -etc

--- app/src/main/java/com/cj3dreams/majorpay/MainActivity.kt
+++ app/src/main/java/com/cj3dreams/majorpay/MainActivity.kt
@@ -2,10 +2,23 @@ package com.cj3dreams.majorpay
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.cj3dreams.majorpay.view.ui.HomeFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        with(window){
+            clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+            setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION,
+                WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION)
+            addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        }
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.mainFrgView, HomeFragment())
+            .commit()
     }
 }
